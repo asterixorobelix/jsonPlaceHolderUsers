d293cc1fe8d658cff1f7161a78b3dcf1e5e8e17f
Unknown <nathanstasin@gmail.com>
2019-05-17 11:22:33 +0200

    add databinding

--- app/src/main/java/asterixorobelix/jsonplaceholderusers/MainActivity.kt
+++ app/src/main/java/asterixorobelix/jsonplaceholderusers/MainActivity.kt
@@ -2,11 +2,24 @@ package asterixorobelix.jsonplaceholderusers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import asterixorobelix.jsonplaceholderusers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var viewBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        viewBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main
+        )
+        
+        viewBinding.usersButton.setOnClickListener {
+            val toast = Toast.makeText(applicationContext, "Clicked!", Toast.LENGTH_LONG)
+            toast.show()
+        }
     }
 }
