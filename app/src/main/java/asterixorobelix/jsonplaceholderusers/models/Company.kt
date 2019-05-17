1f053710542c43df047d22fd1b96b620f9c347a6
Unknown <nathanstasin@gmail.com>
2019-05-17 08:40:58 +0200

    add models

---
+++ app/src/main/java/asterixorobelix/jsonplaceholderusers/models/Company.kt
@@ -0,0 +1,7 @@
+package asterixorobelix.jsonplaceholderusers.models
+
+data class Company (
+    val name : String,
+    val catchPhrase : String,
+    val bs : String
+)
---
+++ app/src/main/java/asterixorobelix/jsonplaceholderusers/models/Geo.kt
@@ -0,0 +1,6 @@
+package asterixorobelix.jsonplaceholderusers.models
+
+data class Geo (
+    val lat : Double,
+    val lng : Double
+)
